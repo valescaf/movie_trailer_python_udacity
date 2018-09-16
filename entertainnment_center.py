a4f049eb5b5b72ef448956e9021ebd5f5e11786a
valescaf <valesca@gmail.com>
2018-09-16 19:43:32 -0300

    Add API with Flask

--- entertainnment_center.py
+++ entertainnment_center.py
@@ -1,10 +1,16 @@
-import fresh_tomatoes
+# Import Media and Fresh Tomatoes
 import media
-
+import fresh_tomatoes
+# Import JSON
+import json
+# Import Flask
+from flask import Flask
+from flask import jsonify
 
 # Make the movie instances
 toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toy that come to live",                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "A story of a boy and his toy that come to live",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 avatar = media.Movie("Avatar", "A Marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
@@ -27,8 +33,23 @@ hunger_games = media.Movie("Hunger Games",
                            "http://www.youtube.com/watch?v=PbA63a7H0bo")
 
 # Creating a list of Objects for passing the movies object to
-# the Fresh Tomatoes File to display
+# The Fresh Tomatoes File to display
 movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+
+# Create a string by list
+movies_string = json.dumps(movies, default=lambda x: x.__dict__)
+
 # Calling the funtion present in fresh_tomatoes to display the
 # movie list and show trailer in Webbrowser
 fresh_tomatoes.open_movies_page(movies)
+
+# Create API
+app = Flask(__name__)
+
+
+@app.route("/movies")
+def Movies_json():
+    return (movies_string)
+
+if __name__ == "__main__":
+    app.run()
