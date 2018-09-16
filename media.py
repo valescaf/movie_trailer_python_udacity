1d9a0f0ad5bdd771d412d29a80e1ffc94fcaa800
valescaf <valesca@gmail.com>
2018-09-16 19:47:47 -0300

    PEP8 requirements

--- media.py
+++ media.py
@@ -7,7 +7,7 @@ class Movie():
         # title : Movie name
         # storyline : Gist of movie
         # poster_image_url : Movie Poster
-        # trailer_youtube   : Youtube Trailer
+        # trailer_youtube : Youtube Trailer
         def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
             self.title = movie_title
             self.storyline = movie_storyline
